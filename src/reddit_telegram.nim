4ce8f3da32219712a6525bc7637afa208542a3a4
Jeff Sandberg <paradox460@gmail.com>
2020-10-11 13:51:45 -0600

    Write cmdparser and daemonizing core

--- src/reddit_telegram.nim
+++ src/reddit_telegram.nim
@@ -1,9 +1,15 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
 import reddit_telegram/reddit
 import reddit_telegram/telegram
 
-import sequtils, strformat, times
+import logging, os, strformat, times
+
+const NimblePkgVersion {.strdefine.} = ""
+
+var
+  lastRun: Time = getTime()
+  logger = newConsoleLogger(fmtStr = verboseFmtStr)
+
+addHandler(logger)
 
 proc buildTelegramMessage(data: RedditPost): string =
   let
@@ -17,13 +23,37 @@ proc buildTelegramMessage(data: RedditPost): string =
 [link]({url}) • [comments]({permalink})
   """
 
+proc run(subreddit, botKey, chatId: string; minScore: int = 5;
+    interval: int = 60; verbose: bool = false): void =
+  if verbose:
+    setLogFilter(lvlAll)
+
+  info("Starting bot")
+  initTelegram(botKey)
+
+  while true:
+    info("Grabbing posts")
+    for post in getNew(subreddit).postFilter(minScore, lastRun):
+      let tgMessage = post.buildTelegramMessage()
+      info(&"Posting: {tgMessage}")
+      sendMessage(chatId, tgMessage)
+    lastRun = getTime()
+    sleep(interval * 1000)
+
 
 when isMainModule:
-  initTelegram("")
-  # TODO: Make this interval configurable
-  let ten_ago = getTime() - initDuration(minutes = 10)
-  # TODO: Add configuration for subreddit, minScore
-  for post in getNew("").postFilter(5, ten_ago):
-    let tgMessage = post.buildTelegramMessage()
-    # TOOD: Add configuration for tg chat id
-    sendMessage("", tgMessage)
+  import cligen
+  include cligen/mergeCfgEnv
+  clCfg.version = NimblePkgVersion
+
+  dispatch(
+    run,
+    cmdName = "reddit_telegram",
+    help = {
+      "subreddit": "subreddit (singular) from which to fetch",
+      "minScore": "minimum score a post can have to be sent to telegram",
+      "botKey": "Telegram bot key. Don't add the `bot` prefix",
+      "chatId": "Telegram channel id or name. ID is safest",
+      "interval": "interval, in seconds, to check reddit for new posts"
+    }
+  )
